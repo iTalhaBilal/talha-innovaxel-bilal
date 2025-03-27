0bb8eb2151010befd058246a7813bc99d8aabde3
Talha Bilal <talhabilalofficial@gmail.com>
2025-03-27 23:11:19 +0500

    POST API working with PostMan and  MongoDb

---
+++ connect.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+mongoose.set("strictQuery", true)
+async function connectToDb(url) {
+    return mongoose.connect(url)
+}
+
+module.exports = { connectToDb }
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const urlRoute = require("./routes/urlRoute")
+const { connectToDb } = require("./connect")
 
-
+connectToDb("mongodb://localhost:27017/shortUrls").then(() => console.log("MongoDb Connected!")).catch((e) => console.log(`Error connecting to DB: ${e}`))
+const PORT = 8001;
 const app = express()
+app.use(express.json())
+
 app.use("/url", urlRoute)
+app.listen(PORT, () => console.log("Server Started!"))
