bdb5ade1477fdc47159fc33239b7dffd9e636525
Talha Bilal <talhabilalofficial@gmail.com>
2025-03-28 18:08:59 +0500

    Update Short URL Done with PUT API

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const urlRoute = require("./routes/urlRoute")
 const URL = require("./model/schema");
+const shortyid = require('shortid');
+
 
 const { connectToDb } = require("./connect")
 
@@ -14,8 +16,23 @@ app.get('/:shortId', async (req, res) => {
     const entry = await URL.findOneAndUpdate({
         shortCode: shortId
     }, { $inc: { accessCount: 1 } })
-    res.redirect(entry.url)
+    if (!entry) return res.status(404)
+    return res.status(201).json({ data: entry })
+
 })
 
-app.use("/url", urlRoute)
+app.put('/:shortId', async (req, res) => {
+    const shortId = req.params.shortId
+    const newShortId = shortyid.generate()
+    const updatedUrl = await URL.findOneAndUpdate({
+        shortCode: shortId
+    }, {
+        shortCode: newShortId
+    })
+    if (!updatedUrl) return res.status(404).json({ error: "Short Url Does NOT exist" })
+    console.log('PUT API HIT')
+    return res.status(200).json({ message: `Url Updated from ${shortId} to ${newShortId}`, data: updatedUrl })
+})
+
+app.use("/shorten", urlRoute)
 app.listen(PORT, () => console.log("Server Started!"))
